9f824afe49199632cb8a38f58a9705ec0002a372
chsherpa <chsherpa@pdx.edu>
2020-10-17 22:08:50 -0700

    Moved datalist into another file for testing

--- P_APP/app.js
+++ P_APP/app.js
@@ -1,47 +1,3 @@
-//Source: https://jsfiddle.net/meziantou/b04mx19f/ from https://www.meziantou.net/html-multiple-selections-with-datalist.htm, 10/12/2020
-/*Allow multiple selects from a data select list while showing it in the UI */
-document.addEventListener("DOMContentLoaded", function () {
-    const separator = ',';
-    for (const input of document.getElementsByTagName("input")) {
-        if (!input.multiple) {
-            continue;
-        }
-        if (input.list instanceof HTMLDataListElement) {
-            const optionsValues = Array.from(input.list.options).map(opt => opt.value);
-            let valueCount = input.value.split(separator).length;
-            input.addEventListener("input", () => {
-                const currentValueCount = input.value.split(separator).length;
-                /*
-                Do not update list if the user doesn't add/remove a separator
-                Current value: "a, b, c"; New value: "a, b, cd" => Do not change the list
-                Current value: "a, b, c"; New value: "a, b, c," => Update the list
-                Current value: "a, b, c"; New value: "a, b" => Update the list
-                */
-                if (valueCount !== currentValueCount) {
-                    const lsIndex = input.value.lastIndexOf(separator);
-                    const str = lsIndex !== -1 ? input.value.substr(0, lsIndex) + separator : "";
-                    filldatalist(input, optionsValues, str);
-                    valueCount = currentValueCount;
-                }
-            });
-        }
-    }
-    function filldatalist(input, optionValues, optionPrefix) {
-        const list = input.list;
-        if (list && optionValues.length > 0) {
-            list.innerHTML = "";
-            const usedOptions = optionPrefix.split(separator).map(value => value.trim());
-            for (const optionsValue of optionValues) {
-                if (usedOptions.indexOf(optionsValue) < 0) {
-                    const option = document.createElement("option");
-                    option.value = optionPrefix + optionsValue;
-                    list.append(option);
-                }
-            }
-        }
-    }
-});
-
 //Source: https://developer.mozilla.org/en-US/docs/Learn/Forms/Sending_forms_through_JavaScript, 10/12/2020
 /* Goal: Send data from Form to another html page for display and printing*/
 /*
@@ -100,9 +56,29 @@ function validateForm(){
 }
 */
    
+/*
 function confirm_reset(){
     return confirm("Are you sure you want to reset all values?");
 }
+*/
+
+/*
+document.addEventListener( "DOMContentLoaded", function() {
+    var data= {};
+    $("#services option").each(function(i,el)
+    {
+    data[$(el).data("value")] = $(el).val();
+    });
+
+    console.log(data, $("#services option").val());
+    $('#submit').click(function()
+    {
+        var value = $('#selected').val();
+        alert($('#services[ value="'+value +'"]'.data('value')));
+    });
+});
+*/
+
 
 /* Paraphrased from source: https://www.mvcode.com/lessons/display-form-results-with-javascript, 10/13/2020*/
 function results(){
@@ -111,7 +87,7 @@ function results(){
     const email = document.getElementById("email").value;
     const address = document.getElementById("address_street").value + '\n' + document.getElementById("address_city").value
     + ', ' + document.getElementById("address_state").value + ', ' + document.getElementById("address_zip").value;
-    const service = document.getElementById("services").list;
+    const service = document.getElementById("services");
     const service_notes = document.getElementById("service_note").value;
     const payment_method = document.getElementById("payment_method").value;
     const payment_deposit = document.getElementById("payment_deposit").value;
@@ -122,10 +98,6 @@ function results(){
     message = document.write("<h3>Here is your data: </h3>");
     message += document.write( name + "<br/>" + address + "<br/>" + phone + "<br/>" + email + "<br/>");
 
-    for ( i = 0; i < service.length; i++ )
-    {
-        document.write( service[i] );
-    }
     message += document.write( service_notes + "<br/>");
     message += document.write( "Paying via: " + payment_method +"<br />");
     message += document.write( 
@@ -135,7 +107,5 @@ function results(){
         );
     message += document.write( "Will be picked up by: " + picked_up_by + "<br/>");
 
-    document.write( " \<input type=\"reset\" value=\"Reset\" onClick=\"window.location.reload()\"\>"
-    );
+    document.write(message);
 }
-    /*document.getElementById(\"input-from\").reset();\"\>"*/
