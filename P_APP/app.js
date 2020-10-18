3ea00fb07feeaa94ab8dcf276cb18b4ffd24eaa7
chsherpa <chsherpa@pdx.edu>
2020-10-17 23:33:30 -0700

    Got form element to display properly

--- P_APP/app.js
+++ P_APP/app.js
@@ -79,15 +79,122 @@ document.addEventListener( "DOMContentLoaded", function() {
 });
 */
 
+document.addEventListener("DOMContentLoaded", function () {
+    // arguments: reference to select list, callback function (optional)
+  function getSelectedOptions(sel, fn) {
+    var opts = [], opt;
+    
+    // loop through options in select list
+    for (var i=0, len=sel.options.length; i<len; i++) {
+        opt = sel.options[i];
+        
+        // check if selected
+        if ( opt.selected ) {
+            // add to array of option elements to return from this function
+            opts.push(opt);
+            
+            // invoke optional callback function if provided
+            if (fn) {
+                fn(opt);
+            }
+        }
+    }
+    
+    // return array containing references to selected option elements
+    return opts;
+  }
 
+  // example callback function (selected options passed one by one)
+  function callback(opt) {
+      // display in textarea for this example
+      var display = document.getElementById('display');
+      display.innerHTML += opt.value + ', ';
+
+      // can access properties of opt, such as...
+      //alert( opt.value )
+      //alert( opt.text )
+      //alert( opt.form )
+  }
+
+  // anonymous function onchange for select list with id demoSel
+  document.getElementById('services').onchange = function(e) {
+      // get reference to display textarea
+      var display = document.getElementById('display');
+      display.innerHTML = ''; // reset
+      
+      // callback fn handles selected options
+      getSelectedOptions(this, callback);
+      
+      // remove ', ' at end of string
+      var str = display.innerHTML.slice(0, -2);
+      display.innerHTML = str;
+  };
+  /*
+  document.getElementById('input-form').onsubmit = function(e) {
+      // reference to select list using this keyword and form elements collection
+      // no callback function used this time
+      var opts = getSelectedOptions( this.elements['services[]'] );
+
+     
+      //alert( 'The number of options selected is: ' + opts.length ); //  number of selected options
+
+      return false; // don't return online form
+  };
+  */
+  document.getElementById('input-form').onsubmit = function(e){
+    const name = document.getElementById("full_name").value;
+    const phone = document.getElementById("phone_number").value;
+    const email = document.getElementById("email").value;
+
+    const address = document.getElementById("address_street").value + '\n' + document.getElementById("address_city").value 
+    + ', ' + document.getElementById("address_state").value + ', ' + document.getElementById("address_zip").value;
+    const service_notes = document.getElementById("service_note").value;
+    const payment_method = document.getElementById("payment_method").value;
+    const payment_deposit = document.getElementById("payment_deposit").value;
+    const payment_balance = document.getElementById("payment_balance").value;
+    const payment_final = document.getElementById("payment_final").value;
+    const picked_up_by = document.getElementById("picked_up_by").value;
+
+    document.write("<h3>Here is your data: </h3>");
+    document.write("Name: " + name + "<br/>" + "Address: " + address + "<br/>" 
+    + "Phone: " + phone + "<br/>" + "Email: " + email + "<br/>");
+
+    var opts = getSelectedOptions( this.elements['services[]'] );
+    if(opts.length != 0 ){
+      document.write("<br />");
+      for(i = 0; i < opts.length; i++)
+      {
+        document.write("Service " + (i+1) + ": " + opts[i].value + "<br />");
+      }
+    }
+    else{
+      document.write("<br />No service needed")
+      document.write("<br />");
+    }
+ 
+    document.write( "<br />Service Notes: " + service_notes + "<br/>");
+    document.write( "Paying via: " + payment_method +"<br />");
+    document.write( 
+        "Balance is: " + payment_balance + "<br/>"
+        + "Depost is: " + payment_deposit + "<br/>" 
+        + "Total Due: " + payment_final + "<br/>" 
+        );
+    document.write( "Will be picked up by: " + picked_up_by + "<br/>");
+
+    document.write( " \<input type=\"reset\" value=\"Reset\" onClick=\"window.location.reload()\"\>"
+    );
+    return false; //don't return online form
+  }
+});
 /* Paraphrased from source: https://www.mvcode.com/lessons/display-form-results-with-javascript, 10/13/2020*/
+/*
 function results(){
     const name = document.getElementById("full_name").value;
     const phone = document.getElementById("phone_number").value;
     const email = document.getElementById("email").value;
     const address = document.getElementById("address_street").value + '\n' + document.getElementById("address_city").value
     + ', ' + document.getElementById("address_state").value + ', ' + document.getElementById("address_zip").value;
-    const service = document.getElementById("services");
+    //const service = document.getElementById("services");
     const service_notes = document.getElementById("service_note").value;
     const payment_method = document.getElementById("payment_method").value;
     const payment_deposit = document.getElementById("payment_deposit").value;
@@ -96,7 +203,10 @@ function results(){
     const picked_up_by = document.getElementById("picked_up_by").value;
 
     message = document.write("<h3>Here is your data: </h3>");
-    message += document.write( name + "<br/>" + address + "<br/>" + phone + "<br/>" + email + "<br/>");
+    message += document.write("Name: " + name + "<br/>" 
+    + "Address: " + address + "<br/>" 
+    + "Phone: " + phone + "<br/>" 
+    + "Email: " + email + "<br/>");
 
     message += document.write( service_notes + "<br/>");
     message += document.write( "Paying via: " + payment_method +"<br />");
@@ -107,5 +217,13 @@ function results(){
         );
     message += document.write( "Will be picked up by: " + picked_up_by + "<br/>");
 
+    var opts = getSelectedOptions( this.elements['services[]'] );
+    for(i = 0; i < opts.length; i++)
+    {
+      document.write(opts[i].value + "<br />");
+    }
     document.write(message);
+    document.write( " \<input type=\"reset\" value=\"Reset\" onClick=\"window.location.reload()\"\>"
+    );
 }
+*/
