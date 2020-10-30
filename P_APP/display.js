cf293079e233aef0b4ca1ecb9817c2ff0ef47dfa
chsherpa <chsherpa@pdx.edu>
2020-10-29 23:21:02 -0700

    More work done on display to make it invoice like

--- P_APP/display.js
+++ P_APP/display.js
@@ -31,7 +31,7 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     var requiredData = {
-      form_id: "3203492",
+      form_id: "IOJVWG12",
       form_date: "10/20/2020",
       cust_name : "John Smith",
       cust_phone : "403-202-3030",
@@ -42,17 +42,27 @@ document.addEventListener("DOMContentLoaded", function () {
       a_street : "200 Lorem St",
       a_city : "Lorem",
       a_state : "CA",
-      a_zip :38939 
+      a_zip : "38939"
     };
 
-//    var services = serviceObjectBuilder(service);
+
+    var services = {
+      Service_0: "Power Adapter",
+      Service_1: "Purchased as Part(s)",
+      Service_2: "Screen Replacement",
+      Service_3: "Traded Computers",
+      Service_4: "Tune Up",
+      Service_5: "Virus Cleanup",
+      Service_6: "Webcam Test",
+      Service_7: "Wifi Test",
+    };
   
     var additionalDataAdditional = {
       serv_notes : "Nothing",
       p_method : "Cash",
-      p_deposit : 30202,
-      p_balance : 39399,
-      p_final : 202302,
+      p_deposit : "30202",
+      p_balance : "39399",
+      p_final : "202302",
       pickUp : "Owner" 
     };
 
@@ -68,22 +78,62 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if( displayData && displayData.length < 5 ){
         console.log(displayData.length);
-        var requiredInfo = JSON.parse(displayData[0])
-        var addressInfo = JSON.parse(displayData[1]);
-        var serviceInfo = JSON.parese(displayData[2]);
-        var paymentInfo = JSON.parse(displayData[3]);
+        var requiredInfo = displayData[0];
+        var addressInfo = displayData[1];
+        var serviceInfo = displayData[2];
+        var paymentInfo = displayData[3];
+        //var paymentInfo = JSON.parse(displayData[3]);
     }
     else{
         alert("Non-valid data");
     }
 
-    var required = document.getElementById("form_id");
+    /*
+    Modified from link below to loop through object with key
+    Source: https://stackoverflow.com/questions/684672/how-do-i-loop-through-or-enumerate-a-javascript-object, 10/29/2020
+    */
+    function objectLoop ( obj ){
+      for( var key in obj ){
+        console.log("Key: " + key);
+        console.log("Value: " + obj[key]);
+        var rep = document.getElementById( key.toString() );
+        if( rep ){
+          rep.innerHTML = obj[key];
+        }
+      }
+    }
+
+    objectLoop(requiredInfo);
+    objectLoop(addressInfo);
+    objectLoop(paymentInfo);
+
+    /*
+    for( var k in serviceInfo ){
+      console.log("Key: " + )
+    }
+    */
+
+    
+
+    /*
+    for ( var key in requiredInfo ){
+        console.log("Key: " + key);
+        console.log("Value: " + requiredInfo[key]);
+        var required = document.getElementById( key.toString() );
+        if( required ){
+          required.innerHTML += requiredInfo[key];
+        }
+      }
+      */
+    
+    /*
+    var required = document.getElementById();
     required.innerHTML = '';
-    required.innerHTML = requiredInfo.form_id;
+    required.innerHTML = requiredInfo.name;
 
     var required = document.getElementById("form_date");
     required.innerHTML = '';
-    required.innerHTML = requiredInfo.form_date;
+    required.innerHTML = requiredInfo.name;
 
     var required = document.getElementById("full_name");
     required.innerHTML = '';
@@ -92,6 +142,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var required = document.getElementById("phone_num");
     required.innerHTML = '';
     required.innerHTML = requiredInfo.cust_phone;
+    */
     /*
     var requiredData = {
       form_id: id,
