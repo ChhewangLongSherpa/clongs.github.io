3bfd2f2ea36024f29ddae23b7d8fc20f7678f903
Chhewang Sherpa <chsherpa@pdx.edu>
2020-11-06 13:34:00 -0800

    Finished styling for printing @ a4 paper size

--- P_APP/display.js
+++ P_APP/display.js
@@ -1,32 +1,10 @@
 /*
-    var requiredData = {
-      form_id: "3203492",
-      form_date: "10/20/2020",
-      cust_name : "John Smith",
-      cust_phone : "403-202-3030",
-      cust_email : "jsmith@smithers.com"
-     };
-
-    var additionalDataAddress = {
-      a_street : "200 Lorem St",
-      a_city : "Lorem",
-      a_state : "CA",
-      a_zip :38939 
-    };
-
-//    var services = serviceObjectBuilder(service);
-  
-    var additionalDataAdditional = {
-      serv_notes : "Nothing",
-      p_method : "Cash",
-      p_deposit : 30202,
-      p_balance : 39399,
-      p_final : 202302,
-      pickUp : "Owner" 
-    };
-
-    var myData = [requiredData, additionalDataAddress, services, additionalDataAdditional];
-    window.localStorage.setItem('cmms_info', JSON.stringify(myData));
+Read/write from csv
+  write to csv: 2 files, one for customers, one for the
+  transactions - one record per service, so a single 
+  transaction can have many services
+  i.e. trans1, cust_email, serv#
+  read from csv...tbd
     */
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -48,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     var services = {
       Service_0: "Power Adapter",
-      Service_1: "Purchased as Part(s)",
+      Service_1: "Purchased as Part",
       Service_2: "Screen Replacement",
       Service_3: "Traded Computers",
       Service_4: "Tune Up",
@@ -56,10 +34,26 @@ document.addEventListener("DOMContentLoaded", function () {
       Service_6: "Webcam Test",
       Service_7: "Wifi Test",
       Service_8: "Liquid Damage Repair",
+      Service_9: "Power Adapter",
+      Service_10: "Purchased as Part",
+      Service_11: "Screen Replacement",
+      Service_12: "Traded Computers",
+      Service_13: "Tune Up",
+      Service_14: "Virus Cleanup",
+      Service_15: "Webcam Test",
+      Service_16: "Wifi Test",
+      Service_17: "Liquid Damage Repair",
+      Service_18: "Liquid Damage Repair",
+      Service_19: "Liquid Damage Repair",
+      Service_20: "Liquid Damage Repair",
+      Service_21: "Liquid Damage Repair",
+      Service_22: "Liquid Damage Repair",
+      Service_23: "Liquid Damage Repair",
+      Service_24: "Liquid Damage Repair",
     };
   
     var additionalDataAdditional = {
-      serv_notes : "Nothing",
+      serv_notes : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
       p_method : "Cash",
       p_deposit : "30202",
       p_balance : "39399",
@@ -117,7 +111,7 @@ document.addEventListener("DOMContentLoaded", function () {
         re.innerHTML += '<tr class="service_item"><td>'+serviceInfo[key]+'</td>\n'+'<td>'+ 0 +'</td></tr>';
         //re.innerHTML += '<li class="service_item">' + serviceInfo[key] + '</li>';
         //pi.innerHTML += '<li class="service_price">0</li>\n';
-        console.log(re);
+        //console.log(re);
       }
     });
 
